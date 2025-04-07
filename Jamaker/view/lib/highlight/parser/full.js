19439d1aa70ff3887a530d1f528c5a30db4ce799
harne_ <harne_@naver.com>
2025-04-07 18:54:23 +0900

    이거 예외처리 필요 없겠구나

--- Jamaker/view/lib/highlight/parser/full.js
+++ Jamaker/view/lib/highlight/parser/full.js
@@ -1,5 +1,8 @@
 SmiEditor.highlightText = (text, state=null) => {
 	const previewLine = $("<span>").data({ state: state });
+	if (state == null && text.toUpperCase().startsWith("<SYNC ")) {
+		previewLine.addClass("hljs-sync");
+	}
 	
 	/*
 	 * 상태값
