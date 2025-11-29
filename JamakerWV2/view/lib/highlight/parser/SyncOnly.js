2c9cfb105a28a7d232d0e9797be6fd32e1589519
harne_ <harne_@naver.com>
2025-11-29 09:44:25 +0900

    WebView2 전환 예정 솔루션 분리

--- JamakerWV2/view/lib/highlight/parser/SyncOnly.js
+++
@@ -1,7 +0,0 @@
-SmiEditor.highlightText = (text, state=null) => {
-	const previewLine = $("<span>");
-	if (text.toUpperCase().startsWith("<SYNC ")) {
-		previewLine.addClass("hljs-comment hljs-sync");
-	}
-	return previewLine.text(text);
-}
