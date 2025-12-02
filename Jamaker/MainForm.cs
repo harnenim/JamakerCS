a257eaafd52d8c94d5d5e98e1f4c2bfb348781be
harne_ <harne_@naver.com>
2025-12-02 19:38:36 +0900

    컴퓨터에 따라 Alt+메뉴 열 때 경고음 나던 문제 수정

--- Jamaker/MainForm.cs
+++ Jamaker/MainForm.cs
@@ -873,6 +873,11 @@ namespace Jamaker
         private AfterGetString afterGetFileName = null;
         protected override void WndProc(ref Message m)
         {
+            if (m.Msg == 0x0120/*WM_MENUCHAR*/)
+            {
+                m.Result = (IntPtr)(1 << 16);
+                return;
+            }
             if (player != null)
             {
                 string path = player.AfterGetFileName(m);
@@ -880,6 +885,7 @@ namespace Jamaker
                 {
                     afterGetFileName?.Invoke(path);
                     Script("setVideo", path);
+                    return;
                 }
             }
             base.WndProc(ref m);
