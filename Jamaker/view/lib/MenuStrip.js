ddd579cb0f5517025d9d2024c85714240b5c2930
harne_ <harne_@naver.com>
2025-12-06 04:46:32 +0900

    메뉴 활성화 여부 보완

--- Jamaker/view/lib/MenuStrip.js
+++ Jamaker/view/lib/MenuStrip.js
@@ -490,8 +490,9 @@ ContextMenu.prototype.open = function(e, owner) {
 	
 	this.owner = (owner ? owner : owner = document.body);
 
-	this.view.childNodes.forEach((li) => {
-		if (eval(li.perm)) {
+	this.view.childNodes.forEach(async (li) => {
+		console.log(li);
+		if (await eval(li.perm)) {
 			li.tabIndex = 1;
 			li.classList.remove("disable");
 		} else {
