0fbe2eadf4cbcc80c871ebc3b1ff62ae169fb5ad
harne_ <harne_@naver.com>
2025-12-06 04:20:14 +0900

    메뉴 활성화 여부 기능 추가

--- Jamaker/Jamaker.bat
+++ Jamaker/Jamaker.bat
@@ -1,6 +1,8 @@
 @echo off
 start /D"C:\Program Files\Google\Chrome\Application\" chrome.exe ^
---disable-web-security ^
 --user-data-dir=D:\Jamaker\browser ^
+--disable-web-security ^
 --disable-popup-blocking ^
+--unsafely-treat-insecure-origin-as-secure ^
+--allow-file-access-from-files ^
 --app=D:\workspace\VisualStudio\Jamaker\Jamaker\Jamaker.html
