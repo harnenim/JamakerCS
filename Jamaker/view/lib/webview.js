4e70650a0d2e5a29245feb2312dc67bbe17f9fae
harne_ <harne_@naver.com>
2025-12-26 02:26:36 +0900

    백틱 문법 추가 적용

--- Jamaker/view/lib/webview.js
+++ Jamaker/view/lib/webview.js
@@ -276,7 +276,7 @@ Progress.prototype.set = function (value, total) {
 		this.last = now;
 	}
 	this.bar.style.width = `calc(${ ratio * 100 }%)`;
-	this.text.innerText = (`${value}/${total}`);
+	this.text.innerText = `${value}/${total}`;
 	this.div.style.display = "block";
 }
 Progress.prototype.hide = function() {
